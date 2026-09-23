fb28f8dc522c01d24f6ea078e120157ebd4554c9
9dbit <154832153+9dbit@users.noreply.github.com>
2026-09-23 23:52:30 +0700

    feat: add prompt layout preview bridge request

--- sketchup-extension/archon/client.rb
+++ sketchup-extension/archon/client.rb
@@ -9,19 +9,28 @@ module Archon
     module_function
 
     def send_manifest(manifest, &callback)
+      post_json(Archon::Config.manifest_url, manifest, &callback)
+    end
+
+    def preview_layout(prompt, &callback)
+      payload = {
+        prompt: prompt.to_s,
+        projectId: Archon::Config.project_id
+      }
+      post_json(Archon::Config.layout_preview_url, payload, &callback)
+    end
+
+    def post_json(url, payload, &callback)
       raise 'ARCHON_BRIDGE_NOT_CONFIGURED' unless Archon::Config.configured?
 
-      request = Sketchup::Http::Request.new(
-        Archon::Config.manifest_url,
-        Sketchup::Http::POST
-      )
+      request = Sketchup::Http::Request.new(url, Sketchup::Http::POST)
       request.headers = {
         'Content-Type' => 'application/json',
         'Accept' => 'application/json',
         'Authorization' => "Bearer #{Archon::Config.bridge_token}",
         'X-Archon-Client' => "sketchup/#{Archon::EXTENSION_VERSION}"
       }
-      request.body = JSON.generate(manifest)
+      request.body = JSON.generate(payload)
 
       @requests << request
       request.start do |finished_request, response|
